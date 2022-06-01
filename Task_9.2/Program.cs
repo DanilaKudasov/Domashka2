24ce718badda559b57505e5ed7887427ab956bdb
Danila <kudasovdd@gmail.com>
2022-06-02 01:11:27 +0300

    add task_9.2

---
+++ Task_9.2/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Number = {number}");
+int maxNum = FindMaxNumber (number);
+Console.WriteLine($"Max = {maxNum}");
+
+
+int FindMaxNumber (int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
